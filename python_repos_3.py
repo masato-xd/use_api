b769fdeddd5c958dab78493e199c0a35b46cff1d
masato-xd <ddmasato@vip.qq.com>
2017-12-29 23:09:29 +0800

    111
    111

--- python_repos_3.py
+++ python_repos_3.py
@@ -9,11 +9,11 @@ from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS, Style
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
 r = requests.get(url)
 # print dir(r)
-print "Status code:", r.status_code
+print("Status code:", r.status_code)
 
 # 将api响应存储在一个变量中
 response_dict = r.json()
-print "Total repositories:", response_dict['total_count']
+print("Total repositories:", response_dict['total_count'])
 
 # 探索有关仓库的信息,反馈一个列表
 repo_dicts = response_dict['items']
@@ -38,7 +38,7 @@ for repo_dict in repo_dicts:
         }
         plot_dicts.append(plot_dict)
 
-    # 可视化
+# 可视化
 my_style = Style()
 my_style = LS('#333366', base_style=LCS)
 my_style.title_font_size = 12               # 标题大小
